2f97ca11a34984aa5331c9b32397a2ed42a27e69
Gustavo Cardoso <gustavocardoso@gmail.com>
2017-10-18 15:35:35 -0200

    Add new todo function

---
+++ js/app.js
@@ -0,0 +1,18 @@
+(function() {
+  const todoListContainer = document.querySelector('#todos-container');
+  const doneTodoListContainer = document.querySelector('#done-todos-container');
+  const formTodo = document.querySelector('#form-todo');
+  const newTodoText = document.querySelector('#new-todo');
+  const addIcon = document.querySelector('#icon-add');
+
+  let todos = [];
+
+  formTodo.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const newTodo = { text: newTodoText.value, status: false };
+
+    todos.push(newTodo)
+    console.log(todos);
+  }, false);
+})();

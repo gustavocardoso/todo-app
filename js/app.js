0ddf4e51e79d7ee5a66b1e8bc8ba51bf59251fb8
Gustavo Cardoso <gustavocardoso@gmail.com>
2017-10-18 16:04:10 -0200

    Cria lista de novas todos

--- js/app.js
+++ js/app.js
@@ -9,10 +9,57 @@
 
   formTodo.addEventListener('submit', function(event) {
     event.preventDefault();
-
-    const newTodo = { text: newTodoText.value, status: false };
-
-    todos.push(newTodo);
-    newTodoText.value = '';
+    addTodo();
   }, false);
+
+  function addTodo() {
+    if (newTodoText.value !== '') {
+      const newTodo = { text: newTodoText.value, status: false };
+
+      todos.push(newTodo);
+      newTodoText.value = '';
+      console.log(todos);
+
+      renderTodos();
+    }
+  }
+
+  function renderTodos() {
+    newTodoList();
+  }
+
+  function newTodoList() {
+    const oldList = document.querySelector('#todos');
+    const listSize = todos.length;
+    const docFrag = document.createDocumentFragment();
+    const todoList = document.createElement('ul');
+    let showList = false;
+
+    docFrag.appendChild(todoList);
+
+    todoList.setAttribute('id', 'todos');
+
+    if(oldList !== null) {
+      oldList.remove();
+    }
+
+    for(let index = 0; index < listSize; index++) {
+      const listItem = document.createElement('li');
+      const itemLink = document.createElement('a');
+
+      itemLink.setAttribute('data-index', index);
+      itemLink.setAttribute('href', '#');
+      itemLink.classList.add('item-link')
+
+      itemLink.innerHTML = todos[index].text;
+
+      listItem.appendChild(itemLink);
+      todoList.appendChild(listItem);
+      showList = true;
+    }
+
+    if (showList) {
+      todoListContainer.appendChild(docFrag);
+    }
+  }
 })();
